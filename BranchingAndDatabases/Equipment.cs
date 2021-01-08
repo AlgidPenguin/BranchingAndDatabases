e5b67ef403d93b2b444f6d6f446ca4a2d167ab53
Daniel Ramirez <daniel_r129@hotmail.com>
2021-01-07 22:03:50 -0800

    Created computer equipment class

---
+++ BranchingAndDatabases/Equipment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAndDatabases
+{
+    /// <summary>
+    /// Represents a single piece of computer equipment
+    /// </summary>
+    class Equipment
+    {
+        /// <summary>
+        /// Primary key for equipment
+        /// </summary>
+        public int EquipmentId { get; set; }
+
+        /// <summary>
+        /// The name of the equipment
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Retail price of the equipment
+        /// </summary>
+        public double Price { get; set; }
+    }
+}
